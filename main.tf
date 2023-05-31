1c04e79e7365e5368b5df403423d7b081ddbd2c9
Tim Wilson <twilson@gmail.com>
2023-05-31 11:52:17 -0400

    finished vpc

--- main.tf
+++ main.tf
@@ -34,3 +34,22 @@ resource "aws_subnet" "public2" {
     env  = "Dev"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "twilson-publicroutetable"
+    env  = "Dev"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = [aws_subnet.public1.id, aws_subnet.public2.id]
+}
